39b8fd7e45e0e306e96d004144e85df422c9c0c1
Roanna Victorio <roannav@users.noreply.github.com>
2023-10-05 20:40:17 -0700

    add accordion for plot summaries

---
+++ index.js
@@ -0,0 +1,17 @@
+function initAccordion() {
+  let coll = document.getElementsByClassName("collapsible");
+
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+}
+
+initAccordion();
