ee9c0f5b189164334a796d6447558358e56035c6
Roanna Victorio <roannav@users.noreply.github.com>
2023-10-16 18:33:56 -0700

    mv styles from gallery.html to gallery.css.  Use consistent header, navbar, and footer across all pages

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,10 @@
 const navbar = document.getElementById("navbar");
 navbar.innerHTML = `
 <div class="nav" id="navid">
-<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
-<ul class="navbar-container">
+  <ul class="navbar-container">
     <li><a href="characters.html">Characters</a></li>
     <li><a href="plot.html">Plot</a></li>
     <li><a href="scenes.html">Favourite Scenes</a></li>
@@ -12,7 +12,6 @@ navbar.innerHTML = `
     <li><a href="reviews.html">Reviews</a></li>   
     <li><a href="games.html">Games</a></li>         
     <li><a href="gallery.html">Photo Gallery</a></li>      
-
   </ul>
 </div>
 
