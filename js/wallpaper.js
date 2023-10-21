1b4b6f8a4b96b673ca1fdd4194e75da42f53d621
Roanna Victorio <roannav@users.noreply.github.com>
2023-10-20 22:53:44 -0700

    adjust downloadFile()

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -2,32 +2,34 @@
 
 const downloadButtons = document.getElementsByClassName("wallpaper-download");
 
-for(let i = 0; i < downloadButtons.length; i++){
-    const button = downloadButtons[i]
-    const filename = button.name
-    button.addEventListener("click", () =>{downloadFile(filename)})
-
+for (let i = 0; i < downloadButtons.length; i++) {
+  const button = downloadButtons[i];
+  const filename = button.name;
+  button.addEventListener("click", () => {
+    downloadFile(filename);
+  });
 }
-// downloadButtons.forEach(button => {
-//     const filename = button.name
-//     button.addEventListner("click", () =>{donwloadFile(filename)})
-// });
 
+// defining the downloadFile function
+function downloadFile(fileName) {
+  const filePath = "/img/wallpapers/" + fileName;
 
-// defining the downloadFile function 
-function downloadFile(fileName){
-    const filePath = "../img/wallpapers/" + fileName;
+  const downloadFileName = fileName;
 
-    const downloadFileName = fileName;
+  const a = document.createElement("a");
+  a.style.display = "none";
+  a.href = filePath;
+  a.download = downloadFileName;
 
-    const a = document.createElement("a");
-    
-    a.href = filePath;
+  // add link to the DOM, so it can be clicked
+  document.body.appendChild(a);
 
-    a.download = downloadFileName;
+  a.click();
+  console.log("downloading in progress.");
 
-    a.click()
-    console.log("downloading in progress.")
+  // To make this work on Firefox we need to wait
+  // a little while before removing it.
+  setTimeout(() => {
     document.body.removeChild(a);
-
+  }, 500);
 }
